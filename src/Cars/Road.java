2869c59aaa7cbd49e39604cb225f6865c9533a71
Georgi Ignatov <ginchothetree@gmail.com>
2020-05-27 03:56:19 +0300

    Last ones for today.

--- src/Cars/Road.java
+++ src/Cars/Road.java
@@ -11,14 +11,23 @@ import java.util.Random;
 
 public class Road extends JPanel implements ActionListener, KeyListener {
     private int space;
-    private int width = 50;
-    private int height = 100;
+    public final static int CAR_WIDTH = 50;
+    public final static int CAR_HEIGHT = 100;
     private int speed;
     public final static int PANEL_WIDTH = 1000;
     public final static int PANEL_HEIGHT = 800;
     private int moveStep = 20;
     private ArrayList<Rectangle> oppositeCars;
     private Rectangle car;
+
+    public Rectangle getCar() {
+        return car;
+    }
+
+    public ArrayList<Rectangle> getOppositeCars() {
+        return oppositeCars;
+    }
+
     private Random random;
     boolean gameIsActive = true;
     GameObserver gameObserver = new GameObserver();
@@ -29,7 +38,7 @@ public class Road extends JPanel implements ActionListener, KeyListener {
         t = new Timer(2, this);
         random = new Random();
         oppositeCars = new ArrayList<>();
-        car = new Rectangle(PANEL_WIDTH / 2 - 90, PANEL_HEIGHT - 200, width, height);
+        car = new Rectangle(PANEL_WIDTH / 2 - 90, PANEL_HEIGHT - 200, CAR_WIDTH, CAR_HEIGHT);
         space = 300;
         speed = 2;
         addKeyListener(this);
@@ -46,15 +55,15 @@ public class Road extends JPanel implements ActionListener, KeyListener {
             x = PANEL_WIDTH / 2 - 150;
         } else if (positionX == 1) {
             x = PANEL_WIDTH / 2 - 50;
-        } else if (positionX == 2){
+        } else if (positionX == 2) {
             x = PANEL_WIDTH / 2 + 50;
-        } else if (positionX == 3){
+        } else if (positionX == 3) {
             x = PANEL_WIDTH / 2 + 150;
         }
         if (hasCarAppeared) {
-            oppositeCars.add(new Rectangle(x, 50 - (oppositeCars.size() * space), width, height));
+            oppositeCars.add(new Rectangle(x, 50 - (oppositeCars.size() * space), CAR_WIDTH, CAR_HEIGHT));
         } else {
-            oppositeCars.add(new Rectangle(x, oppositeCars.get(oppositeCars.size() - 1).y - 400, width, height));
+            oppositeCars.add(new Rectangle(x, oppositeCars.get(oppositeCars.size() - 1).y - 400, CAR_WIDTH, CAR_HEIGHT));
         }
     }
 
@@ -67,18 +76,19 @@ public class Road extends JPanel implements ActionListener, KeyListener {
         g.setColor(Color.white);
         g.drawLine(PANEL_WIDTH / 2, 0, PANEL_WIDTH / 2, PANEL_HEIGHT);
         g.setColor(Color.magenta);
-        drawImage(g,car, "src\\Images\\myCar.JPG");
-        for (Rectangle rect : oppositeCars) {
+        drawImage(g, getCar(), "src\\Images\\myCar.JPG");
+        for (Rectangle rect : getOppositeCars()) {
             drawImage(g, rect, "src\\Images\\otherCar.JPG");
         }
     }
 
-    public void drawImage (Graphics g, Rectangle rec, String path) {
+    public void drawImage(Graphics g, Rectangle rec, String path) {
         Graphics2D graph = (Graphics2D) g;
         ImageIcon imageIcon = new ImageIcon(path);
         Image image = imageIcon.getImage();
-        graph.drawImage(image, rec.x, rec.y, width, height, null);
+        graph.drawImage(image, rec.x, rec.y, CAR_WIDTH, CAR_HEIGHT, null);
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -111,8 +121,8 @@ public class Road extends JPanel implements ActionListener, KeyListener {
     public void moveUp() {
         if (car.y - moveStep > 0) {
             car.y -= moveStep;
-            if (car.y < (PANEL_HEIGHT / 2 - height/2)) {
-                car.y = PANEL_HEIGHT/ 2 - height/2;
+            if (car.y < (PANEL_HEIGHT / 2 - CAR_HEIGHT / 2)) {
+                car.y = PANEL_HEIGHT / 2 - CAR_HEIGHT / 2;
             }
         }
     }
@@ -120,8 +130,8 @@ public class Road extends JPanel implements ActionListener, KeyListener {
     public void moveDown() {
         if (car.y + moveStep + car.height < PANEL_HEIGHT - 1) {
             car.y += moveStep;
-            if (car.y > PANEL_HEIGHT - 2*height) {
-                car.y = PANEL_HEIGHT - 2*height;
+            if (car.y > PANEL_HEIGHT - 2 * CAR_HEIGHT) {
+                car.y = PANEL_HEIGHT - 2 * CAR_HEIGHT;
             }
         }
     }
@@ -136,16 +146,17 @@ public class Road extends JPanel implements ActionListener, KeyListener {
     }
 
     public void moveRight() {
-        if (car.x + moveStep < PANEL_WIDTH /2 + 160) {
+        if (car.x + moveStep < PANEL_WIDTH / 2 + 160) {
             car.x += moveStep;
-            if (car.x > PANEL_WIDTH /2 + 160) {
-                car.x = PANEL_WIDTH /2 + 160;
+            if (car.x > PANEL_WIDTH / 2 + 160) {
+                car.x = PANEL_WIDTH / 2 + 160;
             }
         }
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
@@ -158,7 +169,8 @@ public class Road extends JPanel implements ActionListener, KeyListener {
         int key = e.getKeyCode();
         checkKey(key);
     }
-    public void checkKey (int key){
+
+    public void checkKey(int key) {
         switch (key) {
             case KeyEvent.VK_UP:
                 moveUp();
